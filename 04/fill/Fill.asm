3f32bb45a8b1aaa1436298f280a8f5585e0cf5f5
towa1204 <lsadsfj@gmail.com>
2023-05-04 18:44:22 +0900

    入出力操作プログラムの実装

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,64 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// 塗りつぶし終えるまでキーボード入力を受け付けない仕様にした
+// CPUEmulatorのAnimateをNo Animationにして動かすと動作確認できる
+
+// SCREENENDという変数を作ってスクリーン範囲の最後のアドレスを代入
+    @24575          // スクリーン範囲の最後のアドレス
+    D=A
+    @SCREENEND
+    M=D
+// CNTというカウンタ変数を作ってスクリーン範囲の最初のアドレスを代入
+    @SCREEN
+    D=A
+    @CNT
+    M=D
+
+(LOOP)
+// キーボード入力値の確認
+    @KBD
+    D=M
+    @WHITE
+    D;JEQ
+(BLACK)
+    // すでに塗りつぶし終えていたら（SCREENEND - CNT < 0）
+    // キーボード入力を受け付ける
+    @SCREENEND
+    D=M
+    @CNT
+    D=D-M       
+    @LOOP
+    D;JLT
+
+    // CNTに1を加算する
+    @CNT
+    M=M+1
+    // 黒で塗りつぶす
+    @CNT
+    A=M
+    M=-1
+
+    @BLACK
+    0;JMP
+(WHITE)
+    // すでに塗りつぶし終えていたら（CNT - SCREEN < 0）
+    // キーボード入力を受け付ける
+    @CNT
+    D=M
+    @SCREEN
+    D=D-A       
+    @LOOP
+    D;JLT
+
+    // CNTに1を減算する
+    @CNT
+    M=M-1
+    // 白で塗りつぶす
+    @CNT
+    A=M
+    M=0
+
+    @WHITE
+    0;JMP
